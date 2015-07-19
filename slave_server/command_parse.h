5ff247f1aab9b11535c1145bd10af2fc83c889d8
ruiwng <ruiwng@zju.edu.cn>
2015-07-19 19:21:35 +0800

    command parse

--- slave_server/command_parse.h
+++ slave_server/command_parse.h
@@ -7,12 +7,6 @@
 #ifndef   COMMAND_PARSE_H
 #define   COMMAND_PARSE_H
 
-// delete a file
-void delete_file(char *command_line);
-// download files from the slave server
-void *download_thread(void *);
-// upload files to the slave server
-void *upload_thread(void *);
 //parse the command
 void command_parse(const char *command_line);
 
