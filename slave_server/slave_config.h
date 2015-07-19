f291e838361e35accb8707d33d9cb97aef29c17c
ruiwng <ruiwng@zju.edu.cn>
2015-07-19 19:23:09 +0800

    slave config

---
+++ slave_server/slave_config.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: slave_config.h
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Sat 18 Jul 2015 08:52:50 PM CST
+ ************************************************************************/
+#ifndef   SLAVE_CONFIG_H
+#define   SLAVE_CONFIG_H
+
+//read the slave configure file, include the port of the slave server, the status port of the slave 
+//server, the transmit port of the client, certificate file of 
+//the ssl, the private key of SSL, and the directory to store files.
+//if success return zero, otherwise return -1.
+
+int slave_configure(char *slave_port, char *slave_status_port, char *client_transmit_port, 
+		char *ssl_certificate, char *ssl_key,char *store_dir);
+
+#endif  // SLAVE_CONFIG_H
