21bf4333931ab74a5735f5e10640065e804a98be
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:21:04 +0800

    msg queue

---
+++ master_server/msg_queue.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+	> File Name: message_queue.cpp
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Fri 17 Jul 2015 06:48:26 PM CST
+ ************************************************************************/
+#include  "msg_queue.h"
+
+//constructor function
+msg_queue::msg_queue()
+{
+	sem_init(&queue_sem, 0, 0);
+    pthread_mutex_init(&queue_mutex, NULL);
+}
+
+//destructor function
+msg_queue::~msg_queue()
+{
+	sem_destroy(&queue_sem);
+	pthread_mutex_destroy(&queue_mutex);
+}
+
+void msg_queue::pop_msg(SSL *s, char *message)
+{
+	sem_wait(&queue_sem);//wait until the queue is not empty
+
+	pthread_mutex_lock(&queue_mutex);// lock the mutex
+	const msg &temp = all_messages.front();
+	s = temp.ssl;
+	strcpy(message, temp.message.c_str());
+	all_messages.pop();// get the message out of the queue
+	pthread_mutex_unlock(&queue_mutex);// unlock the mutex
+}
+
+void msg_queue::push_msg(SSL *s, char *message)
+{
+	pthread_mutex_lock(&queue_mutex);// lock the mutex 
+
+	all_messages.push(msg(s, message));//add a message to the queue
+	pthread_mutex_unlock(&queue_mutex);//unlock the mutex
+
+	sem_post(&queue_sem);
+}
+
+
