6444d1a3c01a1c7f86749e7056a1c9e391d4c71c
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:40:20 +0800

    transmit file

---
+++ client/transmit_file.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: transmit_file.h
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Mon 20 Jul 2015 02:15:22 PM CST
+ ************************************************************************/
+#ifndef   TRANSMIT_FILE_H
+#define   TRANSMIT_FILE_H
+
+// the thread to download a file.
+void *download_thread(void *arg);
+
+// the thread to upload a file.
+void *upload_thread(void *arg);
+
+#endif
