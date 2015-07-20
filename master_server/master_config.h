87ebd5ebc66dc0905323f7612ea90ae227271b56
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:20:34 +0800

    master config

--- master_server/master_config.h
+++ master_server/master_config.h
@@ -13,8 +13,12 @@ using namespace std;
 
 //read the configure file, include the address of the redis database,
 //the port of the redis database, the ip address of all the slave servers.
-//the port of the slave and the port of the master server.
-void master_configure(char *redis_address, char *redis_port,
-		vector<string> &slave_array, char *slave_port, char *master_port);
+//the port of the slave, the port of the master server and the status port of the master server.
+// if success return zero, otherwise return -1.
+
+int master_configure(char *redis_address, char *redis_port,
+		vector<string> &slave_array, char *slave_port, char *master_port,
+		char *slave_status_port, char *master_status_port, char *ssl_certificate,
+		char *ssl_key);
 
 #endif // MASTER_CONFIG_H
