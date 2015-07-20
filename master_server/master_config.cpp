532485873800c435171d2c2cb40ca46981bd3021
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:20:22 +0800

    master config

--- master_server/master_config.cpp
+++ master_server/master_config.cpp
@@ -4,24 +4,28 @@
 	> Mail: ruiwng@zju.edu.cn 
 	> Created Time: Thu 16 Jul 2015 02:48:07 PM CST
  ************************************************************************/
-#include  "csd.h"
+#include  "vscs.h"
 #include  "master_config.h"
 
-void master_configure(char *redis_address, char *redis_port,
+int master_configure(char *redis_address, char *redis_port,
 		vector<string> &slave_array, char *slave_port, char *master_port, 
-		char *slave_status_port, char *master_status_port)
+		char *slave_status_port, char *master_status_port, char *ssl_certificate,
+		char *ssl_key)
 {
 	*redis_address = '\0';
 	*redis_port = '\0';
 	*slave_port = '\0';
 	*master_port = '\0';
-	*status_port = '\0';
 	*slave_status_port = '\0';
 	*master_status_port = '\0';
-	FILE *f_config = fopen(master_configure, "r");
+	*ssl_certificate = '\0';
+	*ssl_key = '\0';
+	FILE *f_config = fopen(master_config_file, "r");
 	if(f_config == NULL)
-		log_quit("master_configure: %s not exists", master_configure);
-
+	{
+		log_msg("master_configure: %s not exists", master_config_file);
+		return -1;
+	}
 	char str[MAXLINE];
 	while(fgets(str, MAXLINE, f_config) != NULL)
 	{
@@ -32,17 +36,26 @@ void master_configure(char *redis_address, char *redis_port,
 		else if(name[0] == '#')// comment line
 			continue;
 		else if(n == 1) //wrong configuration
-			log_quit("master_configure: wrong configuration");
+		{
+			log_msg("master_configure: wrong configuration");
+			return -1;
+		}
 		else if(strcmp(name, "REDIS_ADDRESS") == 0) //configuration of the address of the redis database 
 		{
 			if(*redis_address != '\0')
-				log_quit("master_configure: REDIS_ADDRESS duplicate configured");
+			{
+				log_msg("master_configure: REDIS_ADDRESS duplicate configured");
+		 		return -1;
+			}
 			strcpy(redis_address, conf);
 		}
 		else if(strcmp(name, "REDIS_PORT") == 0)// configuration of the address of the redis port
 		{
 			if(*redis_port != '\0')
-				log_quit("master_configure: REDIS_PORT duplicate configured");
+			{
+				log_msg("master_configure: REDIS_PORT duplicate configured");
+				return -1;
+			}
 			strcpy(redis_port, conf);
 		}
 		else if(strcmp(name, "SLAVE_ADDRESS") == 0)// configuration of the address of slave address
@@ -50,29 +63,66 @@ void master_configure(char *redis_address, char *redis_port,
 		else if(strcmp(name, "SLAVE_PORT") == 0)// configuration of the slave port
 		{
 			if(*slave_port != '\0')
-				log_quit("master_configure: SLAVE_PORT duplicate configured");
+			{
+				log_msg("master_configure: SLAVE_PORT duplicate configured");
+				return -1;
+			}
 			strcpy(slave_port, conf);
 		}
 		else if(strcmp(name, "MASTER_PORT") == 0)// configuration of the master port
 		{
 			if(*master_port != '\0')
-				log_quit("master_configure: MASTER_PORT duplicate configured");
+			{
+				log_msg("master_configure: MASTER_PORT duplicate configured");
+		 		return -1;
+			}
 			strcpy(master_port, conf);
 		}
 		else if(strcmp(name, "SLAVE_STATUS_PORT") == 0) // configuration of the slave status port
 		{
 			if(*slave_status_port != '\0')
-				log_quit("master_configure: SLAVE_STATUS_PORT duplicate configured");
+			{
+				log_msg("master_configure: SLAVE_STATUS_PORT duplicate configured");
+		 		return -1;
+			}
 			strcpy(slave_status_port, conf);
 		}
 		else if(strcmp(name, "MASTER_STATUS_PORT") == 0) // configuration of the master status port
 		{
 			if(*slave_status_port != '\0')
-			   log_quit("master_configure: MASTER_STATUS_PORT duplicate configured");
+			{
+			   log_msg("master_configure: MASTER_STATUS_PORT duplicate configured");
+			   return -1;
+			}
 			strcpy(master_status_port, conf);
 		}
+		else if(strcmp(name, "SSL_CERTIFICATE") == 0) //certificate file of SSL
+		{
+			if(*ssl_certificate != '\0')
+			{
+				log_msg("master_configure: SSL_CERTIFICATE duplicate configured");
+		 		return -1;
+			}
+			strcpy(ssl_certificate, conf);
+		}
+		else if(strcmp(name, "SSL_KEY") == 0) // private key of SSL
+		{
+			if(*ssl_key != '\0')
+			{
+		 		log_msg("master_configure: SSL_KEY duplicate configured");
+				return -1;
+			}
+			strcpy(ssl_key, conf);
+		}
 		else // unknown configuration
-			log_quit("master_configure: unknown configure argument");
+		{
+			log_msg("master_configure: unknown configure argument");
+			return -1;
+		}
 	}
+	if(*redis_address == '\0' || *redis_port == '\0' || slave_array.empty() || *slave_port == '\0'
+			|| *master_port == '\0' || *slave_status_port == '\0' || *master_status_port == '\0')
+		return -1;
+	return 0;
 }
 
