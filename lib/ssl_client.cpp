5067e336d47791c754f9160261201c927e6a8308
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:24:42 +0800

    ssl client

---
+++ lib/ssl_client.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: ssl_client.cpp
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Sun 19 Jul 2015 09:32:42 AM CST
+ ************************************************************************/
+#include "vscs.h"
+
+// if success return SSL*, otherwise return NULL
+SSL* ssl_client(SSL_CTX *ctx, int sockfd)
+{
+	SSL *ssl = SSL_new(ctx);
+
+	if(ssl == NULL)
+	{
+		log_msg("ssl_client: SSL_new error");
+		return NULL;
+	}
+
+	SSL_set_fd(ssl, sockfd);
+	int ret = SSL_accept(ssl);
+
+	if(ret == -1)
+	{
+		log_msg("ssl_client: SSL_accept error");
+		return NULL;
+	}
+
+	return ssl;
+}
