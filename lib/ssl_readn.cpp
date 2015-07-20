e1a60480b19a925ec4a050ab0745da2624d72d99
ruiwng <ruiwng@zju.edu.cn>
2015-07-20 17:25:04 +0800

    ssl readn

---
+++ lib/ssl_readn.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: ssl_readn.cpp
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Mon 20 Jul 2015 09:06:33 AM CST
+ ************************************************************************/
+
+#include  "vscs.h"
+
+// read n characters from ssl*.
+
+int ssl_readn(SSL *ssl, void *vptr, int n)
+{
+	int nleft;
+	int nread;
+	char *ptr = (char *)vptr;
+	nleft = n;
+
+	while(nleft > 0)
+	{
+		if((nread = SSL_read(ssl, ptr, nleft)) < 0)
+		{
+			if(errno == EINTR)
+				nread = 0; /* call SSL_readn() again */
+			else
+				return ERR;
+		}else if(nread == 0)
+			break;
+
+		nleft -= nread;
+		ptr += nread;
+	}
+	return (n - nleft);
+}
