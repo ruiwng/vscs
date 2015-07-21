bdf149db6a0207e75f318f72d97db37399a00ab2
ruiwng <ruiwng@zju.edu.cn>
2015-07-21 22:16:59 +0800

    command parse

---
+++ master_server/command_parse.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: command_parse.h
+	> Author: ruiwng
+	> Mail: ruiwng@zju.edu.cn 
+	> Created Time: Tue 21 Jul 2015 11:29:13 AM CST
+ ************************************************************************/
+#ifndef   COMMAND_PARSE_H
+#define   COMMAND_PARSE_H
+
+// parse the command from the client.
+void command_parse(const SSL *ssl,const char *command_line);
+
+#endif
