cabe2f71c91376bff087f3ab67857bf182864823
ruiwng <ruiwng@zju.edu.cn>
2015-07-24 21:49:49 +0800

    ssl test

--- SSL_test/client.cpp
+++ SSL_test/client.cpp
@@ -1,38 +1,37 @@
 #include  "vscs.h" 
 #include <iostream>
 
+#define    SERVER_CERTIFICATE  "cacert.pem"
+#define    SERVER_KEY          "privatekey.pem"
+
  int main( int argc, char* argv[] ) {
    int ret;
 
-   SSL_CTX* ctx = ssl_client_init();
-
-   int client_socket;
+   SSL_CTX* ctx_client = ssl_client_init();
+   SSL_CTX* ctx_server = ssl_server_init(SERVER_CERTIFICATE, SERVER_KEY);
+   int client_socket, server_socket;
    client_socket = client_connect("127.0.0.1","5003");
    if( client_socket < 0  ) {
      std::cout<<"client_connect error."<<std::endl;
      return -1;
-   }
+    }
+   server_socket = server_listen("5004");
+   SSL* ssl_cli = ssl_client(ctx_client, client_socket);
 
-   SSL*     ssl = ssl_client(ctx, client_socket);
-
-  char     buf [4096];
- ret = SSL_write (ssl, "Hello World!", strlen("Hello World!"));  
- printf("%d\n", ret);
+  char buf[4096];
+  ret = SSL_write(ssl_cli, "Hello World!", strlen("Hello World!"));  
   if( ret == -1 ) {
     std::cout<<"SSL_write error."<<std::endl;
     return -1;
   }
-  ret = SSL_read (ssl, buf, sizeof(buf) - 1);  
+	int accept_socket = accept(server_socket, NULL, NULL);
+	SSL *ssl_ser = ssl_server(ctx_server, accept_socket);
+  ret = SSL_read (ssl_ser, buf, sizeof(buf) - 1);  
   if( ret == -1 ) {
     std::cout<<"SSL_read error."<<std::endl;
     return -1;
   }
   buf[ret] = '\0';
- std::cout<<buf<<std::endl;
-  SSL_shutdown(ssl);  /* send SSL/TLS close_notify */
-
-  close(client_socket);
-  SSL_free (ssl);
-  SSL_CTX_free (ctx);
+  printf("client: %s\n", buf);
   return 0;
 }
--- SSL_test/server.cpp
+++ SSL_test/server.cpp
@@ -11,7 +11,8 @@
 int main( int argc, char* argv[] ) {
   int ret;
 
-  SSL_CTX* ctx = ssl_server_init(SERVER_CERTIFICATE, SERVER_KEY);
+  SSL_CTX* ctx_server = ssl_server_init(SERVER_CERTIFICATE, SERVER_KEY);
+  SSL_CTX* ctx_client = ssl_client_init();
   int listen_socket;
   int accept_socket;
 
@@ -20,17 +21,15 @@ int main( int argc, char* argv[] ) {
     std::cout<<"socket error."<<std::endl;
     return -1;
   }
-  sockaddr_in addr_client;
 
-  socklen_t addr_client_len = sizeof(addr_client);
-    accept_socket = accept (listen_socket, (struct sockaddr*) &addr_client, &addr_client_len);
+    accept_socket = accept (listen_socket, NULL, NULL);
   if( accept_socket < 0  ) {
     std::cout<<"accept error."<<std::endl;
     return -1;
   }
   close(listen_socket);
 
-  SSL *ssl = ssl_server(ctx, accept_socket);
+  SSL *ssl = ssl_server(ctx_server, accept_socket);
   char     buf [4096];
 
   ret = SSL_read (ssl, buf, sizeof(buf) - 1);    
@@ -39,16 +38,14 @@ int main( int argc, char* argv[] ) {
     return -1;
   }
   buf[ret] = '\0';
-  std::cout<<buf<<std::endl;
- 
-  ret = SSL_write (ssl, "I hear you.", strlen("I hear you.")); 
+  printf("%s\n", buf);
+  int client_socket = client_connect("127.0.0.1", "5004");
+  SSL *ssl_cli = ssl_client(ctx_client, client_socket);
+  ret = SSL_write (ssl_cli, "I hear you.", strlen("I hear you.")); 
   if( ret == -1 ) {
     std::cout<<"SSL_write error."<<std::endl;
     return -1;
   }
 
-  close(accept_socket);
-  SSL_free (ssl);
-  SSL_CTX_free (ctx);
   return 0;
 }
